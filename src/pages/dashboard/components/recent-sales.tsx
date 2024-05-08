efa7e30d825e509116cd5aa61ce5001eb170d8d4
ngovanvu2003 <ngovanvu203@gmail.com>
2024-05-09 03:45:24 +0700

    first

---
+++ src/pages/dashboard/components/recent-sales.tsx
@@ -0,0 +1,50 @@
+export function RecentSentEmail() {
+  return (
+    <div className='space-y-5 '>
+      <div className=' rounded-xl border bg-card  text-card-foreground shadow lg:col-span-4'>
+        <div className='col-span-1 flex items-center p-3'>
+          <div className='space-y-1'>
+            <p className='text-sm font-medium leading-none'>Sofia Davis</p>
+            <p className='text-sm text-muted-foreground'>
+              sofia.davis@email.com
+            </p>
+          </div>
+          <div className='ml-auto text-sm font-medium'>19/12/2020</div>
+        </div>
+      </div>
+      <div className=' rounded-xl border bg-card  text-card-foreground shadow lg:col-span-4'>
+        <div className='col-span-1 flex items-center p-3'>
+          <div className='space-y-1'>
+            <p className='text-sm font-medium leading-none'>Sofia Davis</p>
+            <p className='text-sm text-muted-foreground'>
+              sofia.davis@email.com
+            </p>
+          </div>
+          <div className='ml-auto text-sm font-medium'>19/12/2020</div>
+        </div>
+      </div>
+      <div className=' rounded-xl border bg-card  text-card-foreground shadow lg:col-span-4'>
+        <div className='col-span-1 flex items-center p-3'>
+          <div className='space-y-1'>
+            <p className='text-sm font-medium leading-none'>Sofia Davis</p>
+            <p className='text-sm text-muted-foreground'>
+              sofia.davis@email.com
+            </p>
+          </div>
+          <div className='ml-auto text-sm font-medium'>19/12/2020</div>
+        </div>
+      </div>
+      <div className=' rounded-xl border bg-card  text-card-foreground shadow lg:col-span-4'>
+        <div className='col-span-1 flex items-center p-3'>
+          <div className='space-y-1'>
+            <p className='text-sm font-medium leading-none'>Sofia Davis</p>
+            <p className='text-sm text-muted-foreground'>
+              sofia.davis@email.com
+            </p>
+          </div>
+          <div className='ml-auto text-sm font-medium'>19/12/2020</div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,212 @@
+import { Button } from '@/components/custom/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Search } from '@/components/search'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import ThemeSwitch from '@/components/theme-switch'
+import { TopNav } from '@/components/top-nav'
+import { UserNav } from '@/components/user-nav'
+import { Layout, LayoutBody, LayoutHeader } from '@/components/custom/layout'
+import { RecentSentEmail } from './components/recent-sales'
+import { Overview } from './components/overview'
+import { DataTable } from '../tasks/components/data-table'
+import { tasks } from '../tasks/data/tasks'
+import { columns } from '../tasks/components/columns'
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      {/* ===== Top Heading ===== */}
+      <LayoutHeader>
+        <TopNav links={topNav} />
+        <div className='ml-auto flex items-center space-x-4'>
+          <Search />
+          <ThemeSwitch />
+          <UserNav />
+        </div>
+      </LayoutHeader>
+
+      {/* ===== Main ===== */}
+      <LayoutBody className='space-y-4'>
+        <div className='flex items-center justify-between space-y-2'>
+          <h1 className='text-2xl font-bold tracking-tight md:text-3xl'>
+            Dashboard
+          </h1>
+          <div className='flex items-center space-x-2'>
+            <Button>Website</Button>
+          </div>
+        </div>
+        <Tabs
+          orientation='vertical'
+          defaultValue='overview'
+          className='space-y-4'
+        >
+          <div className='w-full  pb-2'>
+            <TabsList>
+              <TabsTrigger value='overview'>Overview</TabsTrigger>
+              <TabsTrigger value='visit'>Visit</TabsTrigger>
+              <TabsTrigger value='reports'>Reports</TabsTrigger>
+              <TabsTrigger value='notifications'>Notifications</TabsTrigger>
+            </TabsList>
+          </div>
+          <TabsContent value='overview' className='space-y-4'>
+            <div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-4'>
+              <Card>
+                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+                  <CardTitle className='text-sm font-medium'>
+                    Total Projects
+                  </CardTitle>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    className='h-4 w-4 text-muted-foreground'
+                  >
+                    <rect width='20' height='14' x='2' y='5' rx='2' />
+                    <path d='M2 10h20' />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className='text-2xl font-bold'>+45</div>
+                  <p className='text-xs text-muted-foreground'>
+                    all your added projects
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+                  <CardTitle className='text-sm font-medium'>
+                    Total category
+                  </CardTitle>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    className='h-4 w-4 text-muted-foreground'
+                  >
+                    <rect width='20' height='14' x='2' y='5' rx='2' />
+                    <path d='M2 10h20' />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className='text-2xl font-bold'>+12</div>
+                  <p className='text-xs text-muted-foreground'>
+                    Your total category added
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+                  <CardTitle className='text-sm font-medium'>Visits</CardTitle>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    className='h-4 w-4 text-muted-foreground'
+                  >
+                    <path d='M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2' />
+                    <circle cx='9' cy='7' r='4' />
+                    <path d='M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75' />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className='text-2xl font-bold'>+2350</div>
+                  <p className='text-xs text-muted-foreground'>
+                    number of people who have visited your website
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+                  <CardTitle className='text-sm font-medium'>
+                    Total contact
+                  </CardTitle>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    className='h-4 w-4 text-muted-foreground'
+                  >
+                    <path d='M22 12h-4l-3 9L9 3l-3 9H2' />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className='text-2xl font-bold'>+573</div>
+                  <p className='text-xs text-muted-foreground'>
+                    total number of people contacting you
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
+            <div className='grid grid-cols-1 gap-4 lg:grid-cols-7'>
+              <Card className='col-span-1 lg:col-span-4'>
+                <CardHeader>
+                  <CardTitle>Overview</CardTitle>
+                </CardHeader>
+                <CardContent className='pl-2'>
+                  <Overview />
+                </CardContent>
+              </Card>
+              <Card className='col-span-1 lg:col-span-3'>
+                <CardHeader>
+                  <CardTitle>Sent Email</CardTitle>
+                  <CardDescription>You made 265 this month.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <RecentSentEmail />
+                </CardContent>
+              </Card>
+            </div>
+          </TabsContent>
+          <TabsContent value='visit' className='space-y-4'>
+            <DataTable data={tasks} columns={columns} />
+          </TabsContent>
+        </Tabs>
+      </LayoutBody>
+    </Layout>
+  )
+}
+
+const topNav = [
+  {
+    title: 'Overview',
+    href: 'dashboard/overview',
+    isActive: true,
+  },
+  {
+    title: 'Customers',
+    href: 'dashboard/customers',
+    isActive: false,
+  },
+  {
+    title: 'Products',
+    href: 'dashboard/products',
+    isActive: false,
+  },
+  {
+    title: 'Settings',
+    href: 'dashboard/settings',
+    isActive: false,
+  },
+]
